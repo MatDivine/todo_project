fb35fa30562aa6c5655bb818050c7a3f5d297b98
MatDivine <khodaei.alireza76@gmail.com>
2025-03-26 16:31:06 +0300

    installed and configured settings of JWT in drf and worked about that

--- todoproject/settings.py
+++ todoproject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -161,3 +162,9 @@ REST_FRAMEWORK = {
 # }
 
 
+
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(seconds=60),
+    "REFRESH_TOKEN_LIFETIME": timedelta(seconds=480)
+}
